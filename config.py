34f140a5fd2f9490a83172a3ac025bfa62204f56
Bot-support <82792336+Bot-support@users.noreply.github.com>
2021-06-07 19:50:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,15 +21,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 que = {}
-SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME")
+SESSION_NAME = getenv("SESSION_NAME", "BQBBsWKYGVMMaPQCT6AWC9-Zau_hOTp6Fq5pfI67pZ-VSmFvtvVs4Zx5c8LNC3ABUsfjxBDAQzhVsNAMrvnbIEZ19Kt-loT7OngL0uWnZE1po5ffotJ4WS7YYyCW2Q8g8PhGpPqn8GBSnVE04p_RcjJzNWIgkSXn2AvwcAkTWOQA0JnkebCuDk7m4MTUIAAjIwz6AtmQgY8gIeXPO6cbH9jTwjvBVgj4pfTsnw7LVRRZ8y-f9YoVQRlEnQPvGAFuHEOPy083tq5Y6XkZrk32QLp5-0dGaDprpvzPNGg74225olX83XsQiPkVWig416owuAb2P3le7MHZg2TWE_DJYA-ZX9XNvAA")
+BOT_TOKEN = getenv("BOT_TOKEN", "1706513392:AAGAvq53BHLW4ywfcGClNbEiflZnS-tevgg")
+BOT_NAME = getenv("BOT_NAME", "patriciaXmusic")
 admins = {}
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "3898418"))
+API_HASH = getenv("API_HASH", "5a82313211da04d63297bd4de436228c")
 
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "700"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1607847356").split()))
