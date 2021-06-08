fa29a87a2044f295888c7fda0238f5679561900b
Bot-support <82792336+Bot-support@users.noreply.github.com>
2021-06-08 06:42:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ from dotenv import load_dotenv
 load_dotenv()
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "BQBBsWKYGVMMaPQCT6AWC9-Zau_hOTp6Fq5pfI67pZ-VSmFvtvVs4Zx5c8LNC3ABUsfjxBDAQzhVsNAMrvnbIEZ19Kt-loT7OngL0uWnZE1po5ffotJ4WS7YYyCW2Q8g8PhGpPqn8GBSnVE04p_RcjJzNWIgkSXn2AvwcAkTWOQA0JnkebCuDk7m4MTUIAAjIwz6AtmQgY8gIeXPO6cbH9jTwjvBVgj4pfTsnw7LVRRZ8y-f9YoVQRlEnQPvGAFuHEOPy083tq5Y6XkZrk32QLp5-0dGaDprpvzPNGg74225olX83XsQiPkVWig416owuAb2P3le7MHZg2TWE_DJYA-ZX9XNvAA")
-BOT_TOKEN = getenv("BOT_TOKEN", "1706513392:AAGAvq53BHLW4ywfcGClNbEiflZnS-tevgg")
+BOT_TOKEN = getenv("BOT_TOKEN", "1838517088:AAHS3lo_oWD3Sn--27Za2dhaNbHyq-c-pl0")
 BOT_NAME = getenv("BOT_NAME", "patriciaXmusic")
 admins = {}
 API_ID = int(getenv("API_ID", "3898418"))
